a4a157ac0f5a28ca1287f435eaeaa497b75a66ee
Gilles FRANCOIS <gilles@sopress.net>
2024-01-26 06:20:52 +0100

    nouveau design

--- wp-content/themes/renovaclaire/gen-style.sh
+++ wp-content/themes/renovaclaire/gen-style.sh
@@ -1,61 +1,48 @@
 #!/bin/bash
 
-# Define the directory and output file
-css_dir="./css"
+# Define the directories and output files
+scss_dir="./scss"
+dist_dir="./dist"
+last_mod_file="./dist/last"
 
+mkdir -p $dist_dir
 
+output_file="./dist/style.scss"
+final_file="./dist/style.css"
 
-# Check for --watch argument
-if [ "$1" == "--watch" ]; then
-    echo "Watching for changes"
+# Get the latest modification time in the scss_dir
+latest_mod=$(find $scss_dir -type f -name '*.scss' -exec stat -c '%Y' {} \; | sort -n | tail -1)
 
-    # Command to execute when a change is detected
-    on_change_command="./gen-style.sh"
-
-    # Debounce delay in seconds
-    debounce_delay=2
-
-    # Last run time initialization
-    last_run=0
-
-    # Watching for changes in the directory
-    inotifywait -m -e modify,create,delete "$css_dir" --format '%w%f %e' |
-        while read file event; do
-            echo "Detected $event on $file"
-
-            # Check if the file name contains 'style.css'
-            if [[ "$file" != *"style"* ]]; then
-                current_time=$(date +%s)
-                if (( current_time - last_run > debounce_delay )); then
-                    eval "$on_change_command"
-                    last_run=$current_time
-                fi
-            fi
-        done
-else
-
-    output_file="./style.css"
-
-
-    echo "" > $output_file
-
-    # Check if global.css exists and add it first
-    if [[ -f "$css_dir/start.css" ]]; then
-        cat "$css_dir/start.css" >> $output_file
+# Check if the last_mod_file exists and compare the modification times
+if [[ -f $last_mod_file ]]; then
+    last_mod=$(cat $last_mod_file)
+    if [[ $latest_mod -le $last_mod ]]; then
+        echo "No new changes in SCSS files."
+        exit 0
     fi
-
-    # Check if global.css exists and add it first
-    if [[ -f "$css_dir/global.css" ]]; then
-        echo "@import \"$css_dir/global.css\";" >> $output_file
-    fi
-
-    # Loop through all CSS files in the directory, excluding global.css
-    for file in $css_dir/*.css; do
-        if [[ "$(basename $file)" != "global.css" ]] && [[ "$(basename $file)" != "start.css" ]]; then
-            echo "@import \"$file\";" >> $output_file
-        fi
-    done
-
-    # echo "Style file created: $output_file"
-    npm run css
 fi
+
+echo "" > $output_file
+
+# Check if start.scss exists and add it first
+if [[ -f "$scss_dir/start.scss" ]]; then
+    cat "$scss_dir/start.scss" >> $output_file
+fi
+
+# Check if global.scss exists and add it first
+if [[ -f "$scss_dir/global.scss" ]]; then
+    cat "$scss_dir/global.scss" >> $output_file
+fi
+
+# Loop through all SCSS files in the directory, excluding start.scss and global.scss
+for file in $scss_dir/*.scss; do
+    if [[ "$(basename $file)" != "global.scss" ]] && [[ "$(basename $file)" != "start.scss" ]]; then
+        cat "$file" >> $output_file
+    fi
+done
+
+echo "Compile the SCSS to CSS"
+sass $output_file:$final_file
+
+# Save the latest modification time
+echo $latest_mod > $last_mod_file
