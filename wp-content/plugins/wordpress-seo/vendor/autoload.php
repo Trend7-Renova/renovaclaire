d94ba7528e9eb78a67e435b9f237124508ffc704
renova <contact@lesfrancois.com>
2025-09-01 12:46:32 +0000

    seo

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit24717d60fe3ac80b7b8a1e349e82ba66::getLoader();
+return ComposerAutoloaderInit91347d4778f0a8704858a500d0acfeb6::getLoader();
